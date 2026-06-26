7dc52795fedea245551704ed0e7e9cdb02077005
Sufiyan <suffiyank509@gmail.com>
2026-06-26 20:26:05 +0530

    DOM: attributes and  insertion

--- dom.js
+++ dom.js
@@ -20,21 +20,57 @@
 // let div = document.querySelector("div");
 // console.log(div);
 
-let h2 = document.querySelector("h2");
-h2.innerText = h2.innerText+" from apna college" //this is called concatination
-console.log(h2);
+// let h2 = document.querySelector("h2");
+// h2.innerText = h2.innerText+" from apna college" //this is called concatination
+// console.log(h2);
 
-let divs = document.querySelectorAll(".box");
-console.dir(divs);
+// let divs = document.querySelectorAll(".box");
+// console.dir(divs);
 
-let idx=0;
-for(div of divs){
-    let print = div.innerText = `this is box ${idx}`;
-    console.log(print);
-    idx++;
-}
+// let idx=0;
+// for(div of divs){
+//     let print = div.innerText = `this is box ${idx}`;
+//     console.log(print);
+//     idx++;
+// }
 
 // divs[0].innerText = "unique val 1"; 
 // divs[1].innerText = "unique val 2";
 // divs[2].innerText = "unique val 3";
 
+let para = document.querySelector("p");
+console.log(para.getAttribute("class"));
+
+let box = document.querySelector("div");
+console.log(box.setAttribute("class","new-box"));
+
+let el = document.createElement("button");
+el.innerText = "click 1!";
+
+let el1 = document.createElement("button");
+el1.innerText = "click 2!";
+
+let el3 = document.createElement("button");
+el3.innerText = "click 3!";
+
+let ball = document.querySelector("#ball");
+
+ball.append(el);
+
+let b1 = document.querySelector("#b1");
+b1.prepend(el1);
+
+let b2 = document.querySelector("#b2");
+b2.before(el3);
+
+//Question solving 
+
+let newButton = document.createElement("button");
+newButton.innerText = "click me!";
+
+newButton.style.backgroundColor = "red";
+newButton.style.color = "white";
+
+let body = document.querySelector("body");
+
+body.append(newButton);
